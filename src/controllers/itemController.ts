64a3c9d1983da27853426733e237a650b99688f1
fandasuba <craigjrobinson@hotmail.co.uk>
2025-06-08 23:22:07 +0100

    More rail fixes

--- src/controllers/itemController.ts
+++ src/controllers/itemController.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
 import { insertItem } from "../models/itemModels";
 
-export const addItems = async (req:Request, res: Response) => {
-    const {id, user_id, item} = req.body
-try {
-    const newItem = await insertItem(id, user_id, item)
-    res.status(200).json(newItem)
-} catch(error){
-    res.status(500).json({error: error})
-}
+export const addItems = async (req: Request, res: Response) => {
+    const { id, user_id, item } = req.body;
+    
+    try {
+        const newItem = await insertItem(id, user_id, item);
+        res.status(200).json(newItem);
+    } catch (error) {
+        console.error("Error in addItems controller:", error);
+        if (error instanceof Error) {
+            res.status(400).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: "Unknown error occurred" });
+        }
+    }
 }
