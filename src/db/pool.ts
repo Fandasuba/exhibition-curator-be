be383ecbfcd8bd17d087cfd829a1cf9fab29cf4e
fandasuba <craigjrobinson@hotmail.co.uk>
2025-06-08 20:40:18 +0100

    Added another fix for Rail

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,22 +1,10 @@
 import dotenv from 'dotenv'
 dotenv.config();
-
 import { Pool } from 'pg';
 
-// console.log({
-//   user: process.env.DB_USER,
-//   password: process.env.DB_USER_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-// });
-
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: String(process.env.DB_USER_PASSWORD),
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 export default pool;
